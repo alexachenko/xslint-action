07069528282d32f944c817a3ffb62c080e62608c
alexachenko <alexa.volodchenko@yandex.ru>
2026-03-25 08:43:20 +0300

    added --suppress options and changed dir argument

--- entry.sh
+++ entry.sh
@@ -2,8 +2,20 @@
 
 set -euo pipefail
 
-dir=$1
-
 cd ${GITHUB_WORKSPACE}
 
-xslint "${dir}"
+arguments=""
+
+for arg in $1
+do
+  arguments="${arguments} ${arg} "
+done
+
+suppress=""
+
+for sup in $2
+do
+  suppress="${suppress} --suppress=${sup} "
+done
+
+xslint "${arguments} ${suppress}"
