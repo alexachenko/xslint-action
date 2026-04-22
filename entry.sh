7bff9490bf574c6bd1c735405892450a4ba25b4a
alexachenko <alexa.volodchenko@yandex.ru>
2026-04-23 01:13:08 +0300

    added an ability to work with default values from xslint

--- entry.sh
+++ entry.sh
@@ -21,19 +21,3 @@ fi
 
 IFS=' '
 xslint "${args[@]}" "${suppress[@]}"
-
-
-set -euo pipefail
-
-cd "${GITHUB_WORKSPACE}"
-
-IFS=$'\n'
-for arg in $1; do
-  args+=("${arg}")
-done
-
-for sup in $2; do
-  suppress+=("--suppress=${sup}")
-done
-IFS=' '
-xslint "${args[@]}" "${suppress[@]}"
